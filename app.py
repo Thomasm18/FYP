affac05a5e11541115e759a706da9433c7d0e07d
thomas <thomasbatam@gmail.com>
2021-04-14 20:35:11 +0530

    modified:   app.py 	modified:   templates/book.html 	modified:   templates/index.html 	modified:   templates/layout.html 	new file:   templates/layout2.html 	new file:   templates/orion.html

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
 @app.route('/index.html')
+@app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/booking')
 def book():
     return render_template('book.html')
+
+@app.route('/timing1')
+def orion():
+    return render_template('orion.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
