554b5e88cef96c880479eca8ed650fe16b587a6b
thomas <thomasbatam@gmail.com>
2021-04-15 20:11:06 +0530

    new file:   __pycache__/test.cpython-38.pyc 	modified:   app.py 	new file:   static/js/jquery.js 	new file:   static/js/script.js 	modified:   templates/index.html 	modified:   templates/layout2.html 	modified:   templates/orion.html 	new file:   test.py

--- app.py
+++ app.py
@@ -1,5 +1,52 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request, jsonify, flash, Response
+from test import test
 app = Flask(__name__)
+
+table = [
+    {
+        'id': '1', 
+        'time': '10 AM',
+        'cost': 'INR 18',
+        'saving': 'INR 1.8'
+    },
+    {
+        'id': '2', 
+        'time': '11 AM',
+        'cost': 'INR 12.6',
+        'saving': 'INR 7.2'
+    },
+    {
+        'id': '3', 
+        'time': '12 PM',
+        'cost': 'INR 12.6',
+        'saving': 'INR 7.2'
+    },
+    {
+        'id': '4', 
+        'time': '1 PM',
+        'cost': 'INR 14.4',
+        'saving': 'INR 5.4'
+    },
+    {
+        'id': '5', 
+        'time': '2 PM',
+        'cost': 'INR 14.4',
+        'saving': 'INR 5.4'
+    },
+    {
+        'id': '6', 
+        'time': '3 PM',
+        'cost': 'INR 12.6',
+        'saving': 'INR 7.2'
+    },
+    {
+        'id': '7', 
+        'time': '4 PM',
+        'cost': 'INR 12.6',
+        'saving': 'INR 7.2'
+    }
+]
+
 
 @app.route('/index.html')
 @app.route('/')
@@ -12,7 +59,13 @@ def book():
 
 @app.route('/timing1')
 def orion():
-    return render_template('orion.html')
+    return render_template('orion.html', table=table)
+
+@app.route('/_bookTime')
+def bookTime():
+    slotId = request.args.get('id', type=int)
+    print(slotId)
+    return str(1)
 
 if __name__ == '__main__':
 	app.run(debug=True)
